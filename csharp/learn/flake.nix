a37d9ddab6b46f5e572b023732a7594e8231dba6
alek <lyudskoy@gmail.com>
2023-12-16 17:32:41 +0600

    nuget path

--- csharp/learn/flake.nix
+++ csharp/learn/flake.nix
@@ -20,6 +20,7 @@
         ];
 
         shellHook = ''
+          export NUGET_PACKAGES="$XDG_CACHE_HOME"/NuGetPackages
           export LD_LIBRARY_PATH="${pkgs.ncurses}/lib:$LD_LIBRARY_PATH"
           export OMNISHARPHOME="$XDG_CONFIG_HOME/omnisharp"
           echo "dotnet version: $(dotnet --version)" | ${pkgs.cowsay}/bin/cowsay | ${pkgs.lolcat}/bin/lolcat
