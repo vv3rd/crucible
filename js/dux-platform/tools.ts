100c16c562482ddb4df6514777b48f8ede7f68ec
alek <lyudskoy@gmail.com>
2023-12-25 21:45:47 +0600

    moved files around

--- js/dux-platform/tools.ts
+++ js/dux-platform/tools.ts
@@ -1,69 +1,5 @@
 import { CircularDependencyError, UnboundTokenError } from "./errors";
 
-console.log("Hello via Bun!");
-
-const TokenType = Symbol();
-
-interface Event<T = string, P = unknown> {
-	type: T;
-	data: P;
-}
-
-export type Token<T extends {}> = symbol & {
-	[TokenType]?: T;
-};
-export namespace Token {
-	export type Value<T extends Token<any>> = T extends Token<infer U> ? U : never;
-
-	export type List<S extends any[]> = {
-		[K in keyof S]: Token<S[K]>;
-	};
-}
-
-interface Subject<V> {
-	subscribe(callback: (state: V) => void): () => void;
-	snapshot(): V;
-}
-
-interface Task {
-	(store: Store<any>): void | Promise<void>;
-}
-
-interface Actor<M, E extends Event> {
-	(model: M, action: E): { next: M; task: Task };
-}
-
-interface Store<E extends Event> {
-	select<T extends {}>(key: Token<T>): Subject<T>;
-	dispatch(action: E): void;
-}
-
-interface Provider {
-	resolve<T extends {}>(token: Token<T>): T;
-}
-namespace Provider {
-	export type BindMethod = <T extends {}>(
-		token: Token<T>,
-		resolver: (...args: unknown[]) => T,
-	) => BindMethod;
-
-	export type Builder = {
-		bind: BindMethod;
-	};
-
-	export type BuilderScope = (builder: Builder) => void;
-}
-
-type ToolMkSelector = {
-	<A extends unknown[], R>(
-		selector: (...args: A) => R,
-	): (...tokens: Token.List<A>) => (...args: A) => R;
-
-	<T extends readonly Token<{}>[]>(
-		...tokens: T
-	): <S extends (...args: { [K in keyof T]: Token.Value<T[K]> }) => any>(selector: S) => S;
-};
-
 const mkTools = () => {
 	const injections = new Map<Function, Token<{}>[]>();
 
@@ -80,7 +16,14 @@ const mkTools = () => {
 			return (selector: any) => mkInjection(tokens, selector);
 		}
 	};
-	const mkSelector = _mkSelector as ToolMkSelector;
+	const mkSelector = _mkSelector as {
+		<A extends unknown[], R>(
+			selector: (...args: A) => R,
+		): (...tokens: Token.List<A>) => (...args: A) => R;
+		<T extends readonly Token<{}>[]>(
+			...tokens: T
+		): <S extends (...args: { [K in keyof T]: Token.Value<T[K]> }) => any>(selector: S) => S;
+	};
 
 	const mkProvider = (builderScope: Provider.BuilderScope) => {
 		let bindings = new Map<Token<{}>, (...args: unknown[]) => unknown>();
@@ -121,7 +64,10 @@ const mkTools = () => {
 
 	const mkToken = <T extends {}>(name: string): Token<T> => Symbol.for(`token:${name}`);
 
-	const mkStore = <M, A extends Event>(actor: Actor<M, A>, model: M): Store<A> => {
+	const mkStore = <M, A extends Event>(
+		actor: Actor<M | undefined, A | Event>,
+		provider: Provider,
+	): Store<A> => {
 		const subscribers = new Set<Function>();
 
 		const store: Store<A> = {
@@ -138,8 +84,16 @@ const mkTools = () => {
 			},
 			dispatch(action) {
 				const { next, task } = actor(model, action);
+				model = next;
+				for (const sub of subscribers) sub(next);
+				task(store);
 			},
 		};
+
+		let { next: model } = actor(undefined, {
+			type: Math.random().toString(36).substring(2),
+		});
+
 		return store;
 	};
 
@@ -151,4 +105,53 @@ const mkTools = () => {
 	};
 };
 
+interface Event<T = string, P = undefined> {
+	type: T;
+	data?: P;
+}
 
+const TokenType = Symbol();
+export type Token<T extends {}> = symbol & {
+	[TokenType]?: T;
+};
+export namespace Token {
+	export type Value<T extends Token<any>> = T extends Token<infer U> ? U : never;
+
+	export type List<S extends any[]> = {
+		[K in keyof S]: Token<S[K]>;
+	};
+}
+
+interface Subject<V> {
+	subscribe(callback: (state: V) => void): () => void;
+	snapshot(): V;
+}
+
+interface Task {
+	(store: Store): void | Promise<void>;
+}
+
+interface Actor<M, E extends Event> {
+	(model: M, action: E): { next: M; task: Task };
+}
+
+interface Store<E extends Event = Event> {
+	select<T extends {}>(key: Token<T>): Subject<T>;
+	dispatch(action: E): void;
+}
+
+interface Provider {
+	resolve<T extends {}>(token: Token<T>): T;
+}
+namespace Provider {
+	export type BindMethod = <T extends {}>(
+		token: Token<T>,
+		resolver: (...args: unknown[]) => T,
+	) => BindMethod;
+
+	export type Builder = {
+		bind: BindMethod;
+	};
+
+	export type BuilderScope = (builder: Builder) => void;
+}
