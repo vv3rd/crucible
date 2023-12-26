14158fda53f866634339d0adf1c3265404588936
alek <lyudskoy@gmail.com>
2023-12-26 13:24:17 +0600

    refactor

--- js/dux-platform/tools.ts
+++ js/dux-platform/tools.ts
@@ -1,61 +1,100 @@
 import { CircularDependencyError, UnboundTokenError } from "./errors";
 
+const { assign } = Object;
+const { isArray } = Array;
+
+const never = (): never => {
+	throw new Error("Never should be called");
+};
+
+type Some = {};
+type Fn<R = void, A extends any[] = []> = (...args: A) => R;
+type SomeFn = Fn<Some, Some[]>;
+type SomeToken = Token<Some>;
+type AnyFn = Fn<any, any[]>;
+
 const mkTools = () => {
-	const injections = new Map<Function, Token<{}>[]>();
-
-	const _mkSelector = (tokensOrSelector: any) => {
-		const mkInjection = (tokens: Token<any>[], selector: Function) => {
-			injections.set(selector, tokens);
-			return selector;
+	const injections = new Map<AnyFn, SomeToken[]>();
+	const mkSelector = (() => {
+		const overload1 =
+			<T extends SomeToken[]>(...tokens: T) =>
+			<R extends Some>(selector: Fn<R, Token.ValueList<T>>) => {
+				injections.set(selector, tokens);
+				return selector;
+			};
+		const overload2 =
+			<R extends Some, A extends Some[]>(selector: Fn<R, A>) =>
+			(...tokens: Token.List<A>) => {
+				injections.set(selector, tokens);
+				return selector;
+			};
+		const mkSelector = (first: SomeToken | SomeFn, ...rest: SomeToken[]) => {
+			switch (true) {
+				case typeof first === "symbol":
+					return overload1(first, ...rest);
+				case typeof first === "function":
+					return overload2(first);
+				default:
+					return never();
+			}
 		};
-		if (typeof tokensOrSelector === "function") {
-			const selector = tokensOrSelector;
-			return (tokens: any) => mkInjection(tokens, selector);
-		} else {
-			const tokens = tokensOrSelector;
-			return (selector: any) => mkInjection(tokens, selector);
+		return mkSelector as typeof overload1 & typeof overload2;
+	})();
+
+	const mkProvider = <M,>(builderScope: Provider.BuilderScope) => {
+		const enum boundTo {
+			model,
+			chain,
 		}
-	};
-	const mkSelector = _mkSelector as {
-		<A extends unknown[], R>(
-			selector: (...args: A) => R,
-		): (...tokens: Token.List<A>) => (...args: A) => R;
-		<T extends readonly Token<{}>[]>(
-			...tokens: T
-		): <S extends (...args: { [K in keyof T]: Token.Value<T[K]> }) => any>(selector: S) => S;
-	};
-
-	const mkProvider = (builderScope: Provider.BuilderScope) => {
-		let bindings = new Map<Token<{}>, (...args: unknown[]) => unknown>();
-
-		const bindMethod: Provider.BindMethod = (token, resolver) => (
-			bindings.set(token, resolver), bindMethod
-		);
-		builderScope({
-			bind: bindMethod,
+		const modelBinding = <T,>(selector: Selector<T>) => ({
+			type: boundTo.model as const,
+			selector,
 		});
+		const chainBinding = <T,>(selector: Fn<T, unknown[]>, dependencies: SomeToken[]) => ({
+			type: boundTo.chain as const,
+			selector,
+			dependencies,
+		});
+		type Selector<T> = (model: M) => T;
+		type Binding<T> = ReturnType<typeof modelBinding<T> | typeof chainBinding<T>>;
 
-		const resolutionChain = new Set<Token<{}>>();
+		const bindings = new Map<SomeToken, Binding<{}>>();
+		const bind: Provider.BindMethod = (token, selector) => {
+			const dependencies = injections.get(selector);
+			if (dependencies) {
+				bindings.set(token, chainBinding(selector, dependencies));
+			} else {
+				bindings.set(token, modelBinding(selector));
+			}
+			return bind;
+		};
 
-		const resolve = <T extends {}>(token: Token<T>): T => {
+		const resolutionChain = new Set<SomeToken>();
+		const resolve = <T extends {}>(token: Token<T>): ((model: M) => T) => {
+			const binding = bindings.get(token) as Binding<T>;
+			if (!binding) {
+				throw new UnboundTokenError(token);
+			}
 			if (resolutionChain.has(token)) {
 				throw new CircularDependencyError(token);
 			}
+			if (binding.type === boundTo.model) {
+				return binding.selector;
+			}
 			try {
 				resolutionChain.add(token);
-				const selector = bindings.get(token);
-				if (!selector) {
-					throw new UnboundTokenError(token);
-				}
-				const dependencies = injections.get(selector) ?? [];
-				const result = selector(...dependencies.map(resolve));
-				return result as T;
+				const selectors = binding.dependencies.map(resolve);
+				return (model) => binding.selector(...selectors.map((sel) => sel(model)));
 			} finally {
 				resolutionChain.delete(token);
 			}
 		};
 
-		const provider: Provider = {
+		builderScope({
+			bind: bind,
+		});
+
+		const provider: Provider<M> = {
 			resolve,
 		};
 
@@ -65,16 +104,17 @@ const mkTools = () => {
 	const mkToken = <T extends {}>(name: string): Token<T> => Symbol.for(`token:${name}`);
 
 	const mkStore = <M, A extends Event>(
-		actor: Actor<M | undefined, A | Event>,
-		provider: Provider,
+		actor: Actor<M, A | Event>,
+		provider: Provider<M>,
 	): Store<A> => {
 		const subscribers = new Set<Function>();
 
 		const store: Store<A> = {
-			select<T extends {}>(token: Token<T>): Subject<T> {
-				const state: Subject<T> = {
+			select<T extends {}>(token: Token<T>): State<T> {
+				const selector = provider.resolve(token);
+				const state: State<T> = {
 					snapshot() {
-						throw new Error("TODO");
+						return selector(model);
 					},
 					subscribe(callback) {
 						return () => {};
@@ -85,7 +125,7 @@ const mkTools = () => {
 			dispatch(action) {
 				const { next, task } = actor(model, action);
 				model = next;
-				for (const sub of subscribers) sub(next);
+				for (const sub of subscribers) sub(model);
 				task(store);
 			},
 		};
@@ -99,7 +139,7 @@ const mkTools = () => {
 
 	return {
 		mkProvider,
-		mkSelector,
+		mkSelector: mkSelector,
 		mkToken,
 		mkStore,
 	};
@@ -116,15 +156,13 @@ export type Token<T extends {}> = symbol & {
 };
 export namespace Token {
 	export type Value<T extends Token<any>> = T extends Token<infer U> ? U : never;
-
-	export type List<S extends any[]> = {
-		[K in keyof S]: Token<S[K]>;
-	};
+	export type ValueList<T extends Token<any>[]> = { [K in keyof T]: Token.Value<T[K]> };
+	export type List<S extends any[]> = { [K in keyof S]: Token<S[K]> };
 }
 
-interface Subject<V> {
-	subscribe(callback: (state: V) => void): () => void;
-	snapshot(): V;
+interface State<M> {
+	subscribe(callback: (state: M) => void): () => void;
+	snapshot(): M;
 }
 
 interface Task {
@@ -132,16 +170,16 @@ interface Task {
 }
 
 interface Actor<M, E extends Event> {
-	(model: M, action: E): { next: M; task: Task };
+	(model: M | undefined, action: E): { next: M; task: Task };
 }
 
 interface Store<E extends Event = Event> {
-	select<T extends {}>(key: Token<T>): Subject<T>;
+	select<T extends {}>(key: Token<T>): State<T>;
 	dispatch(action: E): void;
 }
 
-interface Provider {
-	resolve<T extends {}>(token: Token<T>): T;
+interface Provider<M> {
+	resolve<T extends {}>(token: Token<T>): (model: M) => T;
 }
 namespace Provider {
 	export type BindMethod = <T extends {}>(
