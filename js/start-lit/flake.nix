df5f3a7bca814bdcb67dfe5a68d3b5cae05d20e8
alek <lyudskoy@gmail.com>
2024-01-27 12:03:42 +0600

    removed broken git submodule

--- js/start-lit/flake.nix
+++ js/start-lit/flake.nix
@@ -1,40 +1,38 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs:
+  outputs = { nixpkgs, utils, ... }: utils.lib.eachDefaultSystem (system:
     let
-      system = "x86_64-linux";
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs { inherit system; };
+      npmrc = pkgs.writeTextFile {
+        name = "npmrc";
+        destination = "/npmrc";
+        text = ''
+          prefix=''${XDG_DATA_HOME}/npm
+          cache=''${XDG_CACHE_HOME}/npm
+          init-module=''${XDG_CONFIG_HOME}/npm/config/npm-init.js
+        '';
+      };
+      node = pkgs.nodejs_21;
+      pnpm = node.pkgs.pnpm;
     in
     {
-      devShells.${system}.default =
-        let
-          npmrc = pkgs.writeTextFile {
-            name = "npmrc";
-            destination = "/npmrc";
-            text = ''
-              prefix=''${XDG_DATA_HOME}/npm
-              cache=''${XDG_CACHE_HOME}/npm
-              init-module=''${XDG_CONFIG_HOME}/npm/config/npm-init.js
-            '';
-          };
-          node = pkgs.nodejs_21;
-          pnpm = node.pkgs.pnpm;
-        in
-        pkgs.mkShell {
-          packages = [
-            node
-            pnpm
-            npmrc
-          ];
+      devShell = pkgs.mkShell {
+        packages = [
+          node
+          pnpm
+          npmrc
+        ];
 
-          shellHook = ''
-            pnpm node --version | ${pkgs.cowsay}/bin/cowsay | ${pkgs.lolcat}/bin/lolcat
-            export PATH="./node_modules/.bin:$PATH"
-            export NPM_CONFIG_USERCONFIG="${npmrc}/npmrc"
-          '';
-        };
-    };
+        shellHook = ''
+          pnpm node --version | ${pkgs.cowsay}/bin/cowsay | ${pkgs.lolcat}/bin/lolcat
+          export PATH="./node_modules/.bin:$PATH"
+          export NPM_CONFIG_USERCONFIG="${npmrc}/npmrc"
+        '';
+      };
+    }
+  );
 }
