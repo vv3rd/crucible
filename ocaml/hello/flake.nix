a9ee886f6fe40f2d2209500c7671e98748176770
alek <lyudskoy@gmail.com>
2024-02-10 23:28:39 +0600

    ocaml

---
+++ ocaml/hello/flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, utils, ... }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShell = pkgs.mkShell {
+        packages = [
+          pkgs.ocamlPackages.ocaml-lsp
+          pkgs.ocaml
+        ];
+      };
+    }
+  );
+}
