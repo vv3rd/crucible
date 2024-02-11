1c87f73ea2a5fd9de1df01fa8101656b0d4876c1
alek <lyudskoy@gmail.com>
2024-02-12 00:13:02 +0600

    removed wiki-tube, didn't like it

--- js/wiki-tube/src/index.tsx
+++
@@ -1,8 +0,0 @@
-/* @refresh reload */
-import './globals'
-import '@picocss/pico'
-import { render } from "solid-js/web"
-import App from "./App"
-
-render(() => <App />, document.getElementById("root")!)
-
