1f98a6e2742d51b0a9b019715e217ac9ae4e4211
alek <lyudskoy@gmail.com>
2024-02-19 21:45:26 +0600

    moving folders around

--- js/markline/src/index.ts
+++ js/markline/src/index.ts
@@ -1,2 +1,36 @@
+interface ParseMode {
+  consume(char: string): ParseMode;
+}
 
-console.log('Hello world!')
+let chars = {
+  "!": {},
+  "[": {},
+  "`": {},
+  "*": {},
+};
+
+let inSpanMode: ParseMode = (() => {
+  return {
+    consume(char) {
+      return inSpanMode
+    },
+  };
+})();
+
+let normalMode: ParseMode = (() => {
+  return {
+    consume(char) {
+      return normalMode
+    },
+  };
+})();
+
+let parse = (text: string) => {
+  let mode: ParseMode = normalMode
+
+  for (const char of text) {
+    mode = mode.consume(char)
+  }
+};
+
+console.log("Hello world!");
