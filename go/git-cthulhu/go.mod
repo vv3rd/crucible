eebd6620d074b2b94b64d5fd7ab8c2551e4a2075
alek <lyudskoy@gmail.com>
2024-02-25 11:31:24 +0600

    yaml test cases

--- go/git-cthulhu/go.mod
+++ go/git-cthulhu/go.mod
@@ -9,14 +9,20 @@ require (
 
 require (
 	github.com/cweill/gotests v1.6.0 // indirect
+	github.com/fatih/color v1.16.0 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/go-faker/faker/v4 v4.3.0 // indirect
+	github.com/goccy/go-yaml v1.11.3 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/rivo/uniseg v0.4.3 // indirect
 	golang.org/x/mod v0.12.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/term v0.5.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/tools v0.12.0 // indirect
+	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- go/git-cthulhu/main_test.go
+++ go/git-cthulhu/main_test.go
@@ -2,79 +2,62 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"reflect"
 	"strings"
 	"testing"
+
+	"gopkg.in/yaml.v3"
 )
 
-func TestGetCommitsHistory(t *testing.T) {
-	tests := []struct {
-		name  string
-		given []GitCommit
-		want  []string
-	}{
-		// TODO: define output for 0 commits and 1 commit
-		{
-			name:  "Can draw single branch",
-			given: fakeGitCommits(3),
-			want: []string{
-				"@",
-				"|",
-				"@",
-				"|",
-				"@",
-			},
-		},
-		{
-			name:  "Can draw two branches",
-			given: fakeGitCommits(3),
-			want: []string{
-				"@",
-				"|",
-				"| @",
-				"| |",
-				"@ |",
-				"| |",
-				"@─╯",
-			},
-		},
+type TestCaseJson struct {
+	Name  string `json:"name"`
+	Given []int  `json:"given"`
+	Want  string `json:"want"`
+}
+
+type TestCase struct {
+	Name  string
+	Given []GitCommit
+	Want  []string
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func TestDrawGraph(t *testing.T) {
+	cases := readTestYaml()
+	tests := make([]TestCase, len(cases))
+
+	for i, c := range cases {
+		tests[i] = TestCase{
+			Name:  c.Name,
+			Given: fakeGitHistory(c.Given...),
+			Want:  strings.Split(c.Want, "\n"),
+		}
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := DrawGraph(tt.given); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("DrawGraph() == %v, want %v", got, tt.want)
+		t.Run(tt.Name, func(t *testing.T) {
+			if got := DrawGraph(tt.Given); !reflect.DeepEqual(got, tt.Want) {
+				t.Errorf("DrawGraph() == %v, want %v", got, tt.Want)
 			}
 		})
 	}
 }
 
-func fakeGitCommits(total int) []GitCommit {
-	arr := make([]GitCommit, 0)
-
-	var (
-		count int
-	)
-
-	incHash := func() string {
-		count++
-		return strings.Repeat(fmt.Sprintf("%d", count), 6)
-	}
-
-	arr = append(arr, GitCommit{
-		Hash:    incHash(),
-		Parents: []string{},
-	})
-
-	for i := 0; i < total; i++ {
-		arr = append(arr, GitCommit{
-			Hash:    incHash(),
-			Parents: []string{arr[len(arr)-1].Hash},
-		})
-
-	}
-
-	return arr
+func readTestYaml() []TestCaseJson {
+	var cases []TestCaseJson
+	file, err := os.Open("./tests/DrawGraph.yaml")
+	check(err)
+	textBytes, err := io.ReadAll(file)
+	check(err)
+	yaml.Unmarshal(textBytes, &cases)
+	return cases
 }
 
 func fakeGitHistory(commits ...int) []GitCommit {
@@ -94,7 +77,8 @@ func fakeGitHistory(commits ...int) []GitCommit {
 		Parents: []string{},
 	})
 
-	for _, commitBranchNumber := range commits {
+	for _, i := range commits {
+		_ = i
 		arr = append(arr, GitCommit{
 			Hash:    incHash(),
 			Parents: []string{arr[len(arr)-1].Hash},
