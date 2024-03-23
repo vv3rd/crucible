528345b415aa1cf79ef25948798d93e782be1222
alek <lyudskoy@gmail.com>
2024-03-23 15:26:14 +0500

    (git-cthulhu) added ts to flake

--- go/git-cthulhu/flake.nix
+++ go/git-cthulhu/flake.nix
@@ -1,11 +1,45 @@
 {
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
     tools.url = "github:vv3rd/tools";
   };
 
-  outputs = { utils, tools, ... }: utils.lib.eachDefaultSystem (system:
+  outputs = { nixpkgs, utils, tools, ... }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      npmrc = pkgs.writeTextFile {
+        name = "npmrc";
+        destination = "/npmrc";
+        text = ''
+          prefix=''${XDG_DATA_HOME}/npm
+          cache=''${XDG_CACHE_HOME}/npm
+          init-module=''${XDG_CONFIG_HOME}/npm/config/npm-init.js
+        '';
+      };
+      node = pkgs.nodejs_21;
+      pnpm = node.pkgs.pnpm;
+    in
     {
-      devShell = tools.devShells.${system}.golang;
+      devShell = pkgs.mkShell {
+        packages = [
+          pkgs.bun
+          pkgs.biome
+          node
+          pnpm
+          npmrc
+          pkgs.go
+          pkgs.gopls
+          pkgs.gotools
+          pkgs.entr
+          pkgs.just
+        ];
+
+        shellHook = ''
+          export PATH="$PWD/node_modules/.bin:$PATH"
+          export NPM_CONFIG_USERCONFIG="${npmrc}/npmrc"
+          export GOPATH="$XDG_DATA_HOME"/go
+        '';
+      };
     });
 }
