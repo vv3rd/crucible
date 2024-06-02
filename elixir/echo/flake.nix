73e7e0dd5fa737d44b45089eb248267e20bfff21
alek <lyudskoy@gmail.com>
2024-06-02 22:33:26 +0500

    (elixir) added db to gitignore

--- elixir/echo/flake.nix
+++ elixir/echo/flake.nix
@@ -4,7 +4,7 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, utils, ... }: utils.lib.eachDefaultSystem (system:
+  outputs = { nixpkgs, utils, self, ... }: utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
     in
@@ -16,8 +16,15 @@
           pkgs.erlang
           pkgs.postgresql
           pkgs.inotify-tools
+          pkgs.cowsay
+          pkgs.lolcat
         ];
-        MIX_XDG="true";
+        MIX_XDG = "true";
+
+        shellHook = ''
+          # echo "execute 'docker run --rm --name elixir-pg -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v \$DB_VOLUME:/var/lib/postgresql/data postgres" | cowsay | lolcat
+          export DB_VOLUME="$PWD/db"
+        '';
       };
     }
   );
