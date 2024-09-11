64a8bb7fafd5bf31b5150791156ba959f0fa54fc
vv3rd <lyudskoy@gmail.com>
2024-09-11 23:23:42 +0500

    runOnce util extended

--- playground/js/lib/utils.ts
+++ playground/js/lib/utils.ts
@@ -1,12 +1,22 @@
-export function doNothing() { }
-export function runOnce<T extends (...a: any[]) => any>(func: T) {
-    type A = Parameters<T>;
-    type R = ReturnType<T>;
-    let result: R;
-    let runner = (...args: A): R => {
-        result = func(...args);
-        runner = () => result;
-        return result;
-    };
-    return Object.assign((...args: A) => runner(...args), func) as T;
+export function doNothing() {}
+
+export function runOnce<T extends (...a: any[]) => any>(func: T): T {
+	type A = Parameters<T>;
+	type R = ReturnType<T>;
+	let runner = (...args: A): R => {
+		try {
+			const result = func(...args);
+			runner = () => {
+				return result;
+			};
+			return result;
+		} catch (error) {
+			runner = () => {
+				throw error;
+			};
+			throw error;
+		}
+	};
+	const wrapper = (...args: A) => runner(...args);
+	return Object.assign(wrapper, func);
 }
