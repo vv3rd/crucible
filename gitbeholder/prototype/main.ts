583a36c1ab4e56416e9865ee272022c353c76f8e
vv3rd <lyudskoy@gmail.com>
2024-10-13 09:52:57 +0500

    scetching out gitbeholder

--- gitbeholder/prototype/main.ts
+++ gitbeholder/prototype/main.ts
@@ -6,137 +6,225 @@ export function DrawGraph(history: Git.History): Drawing.Row[] {
 	}
 
 	if (commits.length === 1) {
-		return [{ columns: [Drawing.Column.CommitNode(0)] }];
+		const branch = new Drawing.Branch(commits[0], null);
+		const cell = new Drawing.Cell.CommitNode(branch);
+		return [{ cells: [cell] }];
 	}
 
 	let ongoing = new Set<Drawing.Branch>();
 	let drawing = new Array<Drawing.Row>(commits.length);
 
-	ongoing.add({
-		last: commits[0],
-		rest: null,
-	});
+	ongoing.add(new Drawing.Branch(commits[0], null));
 
 	for (let i = 1; i < commits.length - 1; i += 1) {
-		let thisCommit = commits[i];
-		let children = getChildrenOf(thisCommit);
+		const commit = commits[i];
+		classifyCommit(commit, ongoing);
+	}
 
-		let commitIsParent = children.length > 0;
-		let commitIsHeadOfUmergedBranch = !commitIsParent;
-		let commitIsMergeIn = thisCommit.parents.length === 2;
-		let commitIsDangling = thisCommit.parents.length === 0;
+	return [];
+}
 
-		if (commitIsParent) {
-			let columns = new Array<Drawing.Column>(ongoing.size);
-			// TODO: do something with columns
-			drawing[i] = { columns };
-		} else if (commitIsDangling) {
-			throw new Error(DANGLING_COMMIT_ERROR); // FIXME: figure out how to draw dangling commits
-		} else {
-			// commit is head of unmerged branch
-			if (thisCommit.parents.length === 1) {
+function classifyCommit(
+	thisCommit: Git.Commit,
+	ongoingBranches: ReadonlySet<Drawing.Branch>,
+) {
+	const children = getChildrenOf(thisCommit);
+	const numberOfChildren = children.length;
+	const numberOfParents = thisCommit.parents.length;
+	if (numberOfChildren > -1) absurd("length should not be negative");
+	if (numberOfParents > -1) absurd("length should not be negative");
+
+	switch (numberOfChildren) {
+		case 0: {
+			switch (numberOfParents) {
+				case 0: {
+					/* 
+						 
+					@ <- dangling or the only commit
+             
+					*/
+				}
+				case 1: {
+					/* first commit in a branch
+					@   
+					│   
+					│ */
+				}
+				default: {
+					/* merge of one or more branches as latest commit in them
+					@─╮ <- this or @─╮─╮ <- this and etc.
+					│ │            │ │ │                 
+					│ @            │ @ │                 
+          │ │            │ │ │                 
+          │ │            │ │ @                 
+					*/
+				}
+			}
+		}
+		case 1: {
+			// ? should keep branches same but extend current one ?
+			switch (numberOfParents) {
+				case 0: {
+					/* finalization of branch without creating new ones, first commit in history or in dangling branch
+					│           
+					@           
+					│           
+					@ <- this */
+				}
+				case 1: {
+					/* continuation of a branch
+					│        
+					@ <- this
+					│        
+					*/
+				}
+				default: {
+					/* merge of one or more branches
+					│              │                     
+					@─╮ <- this or @─╮─╮ <- this and etc.
+					│ │            │ │ │                 
+					│ @            │ @ │                 
+          │ │            │ │ │                 
+          │ │            │ │ @                 
+					*/
+				}
+			}
+		}
+		default: {
+			/* start of branches
+			   B   D
+			   │   │
+			 A │   │
+			 │ │   │
+			 │ │ C │
+			 │ │ │ │
+			 ╰─@─╯ │
+			   │   │ */
+			// ? should close all mergin branches (A,B,C not D) and start new one
+
+			switch (numberOfParents) {
+				case 0: {
+					/*
+          @                @  
+          │                │  
+          │              @ │  
+          │              │ │  
+          │ @            │ │ @
+          │ │            │ │ │
+          @─╯ <- this or ╰─@─╯ <- this and etc. */
+				}
+				case 1: {
+					/* same as previous but with a parent
+          │ │            │ │ │
+          @─╯ <- this or ╰─@─╯ <- this and etc. 
+          │                │  
+          @                @  
+          */
+				}
+				default: {
+					/* 
+            │ │              │ │ │   // TODO: find better way to draw this
+          ╭─@─╯ <- this or ╭─╰─@─╯╮╮ <- this and etc. 
+          │ │              │   │ ╭╯│
+          @ │              │   │ @ │
+          │ │              │   │ │ │
+          │ @              │   │ │ @
+          */
+				}
 			}
 		}
 	}
 
-	return [];
-
-	function getChildrenOf(commit: Git.Commit) {
-		return Array.from(ongoing)
-			.filter(({ last: { parents } }) => parents.includes(commit.hash))
-			.map((commit, index) => ({ commit, index }));
+	function getChildrenOf(commit: Git.Commit): readonly Drawing.Branch[] {
+		return Array.from(ongoingBranches).filter(({ latest: { parents } }) =>
+			parents.includes(commit.hash),
+		);
 	}
 }
 
-const DANGLING_COMMIT_ERROR =
-	"Don't know how to draw what appears to be a dangling commit";
-
 export namespace Drawing {
-	enum LineTurn {
-		Right,
-		Left,
+	export type Cell =
+		| Cell.VerticalEdge
+		| Cell.BranchStart
+		| Cell.BranchEnd
+		| Cell.CommitNode
+		| Cell.WhiteSpace;
+
+	export abstract class AnyCell {
+		abstract type: number;
+		constructor(public ownerBranch: Branch) {}
 	}
 
-	enum ColumnType {
-		VerticalEdge,
-		BranchStart,
-		BranchEnd,
-		WhiteSpace,
-		CommitNode,
+	export namespace Cell {
+		export class CommitNode extends AnyCell {
+			static readonly type = 0;
+			readonly type = 0;
+		}
+		export class VerticalEdge extends AnyCell {
+			static readonly type = 1;
+			readonly type = 1;
+		}
+		export class BranchStart extends AnyCell {
+			static readonly type = 2;
+			readonly type = 2;
+		}
+		export class BranchEnd extends AnyCell {
+			static readonly type = 3;
+			readonly type = 3;
+		}
+		export class WhiteSpace extends AnyCell {
+			static readonly type = 4;
+			readonly type = 4;
+		}
 	}
 
-	type AnyColumn = {
-		position: number;
-	};
-
-	export type Column = AnyColumn &
-		(
-			| ReturnType<typeof Column.VerticalEdge>
-			| ReturnType<typeof Column.BranchStart>
-			| ReturnType<typeof Column.BranchEnd>
-			| ReturnType<typeof Column.CommitNode>
-			| ReturnType<typeof Column.WhiteSpace>
-		);
-
-	export namespace Column {
-		export const VerticalEdge = (i: number) =>
-			({
-				position: i,
-				type: ColumnType.VerticalEdge,
-			}) as const;
-		export const BranchStart = (i: number, turn: LineTurn) =>
-			({
-				position: i,
-				type: ColumnType.BranchStart,
-				turn: turn,
-			}) as const;
-		export const BranchEnd = (i: number, turn: LineTurn) =>
-			({
-				position: i,
-				type: ColumnType.BranchEnd,
-				turn: turn,
-			}) as const;
-		export const CommitNode = (i: number) =>
-			({
-				position: i,
-				type: ColumnType.CommitNode,
-			}) as const;
-		export const WhiteSpace = (i: number) =>
-			({
-				position: i,
-				type: ColumnType.WhiteSpace,
-			}) as const;
+	export class Row {
+		constructor(public cells: Array<Cell>) {}
 	}
 
-	export type Row = {
-		columns: Array<Column>;
-	};
-
-	export type Branch = {
-		last: Git.Commit;
-		rest: null | Branch;
-	};
+	export class Branch {
+		column = 0;
+		// cells = new Array<AnyCell>();
+		constructor(
+			public latest: Git.Commit,
+			public rest: null | Branch,
+		) {}
+	}
 
 	export enum Gliph {
 		Node = "@",
 		EdgeV = "│",
 		EdgeH = "─",
-		TurnTR = "╰",
-		TurnBR = "╭",
-		TurnBL = "╮",
-		TurnTL = "╯",
+		TurnNE = "╰",
+		TurnSE = "╭",
+		TurnSW = "╮",
+		TurnNW = "╯",
 	}
 }
 
 export namespace Git {
 	export type Hash = string;
 
-	export type Commit = {
-		hash: Hash;
-		parents: Hash[];
-	};
+	export class Commit {
+		constructor(
+			public hash: Hash,
+			public parents: Hash[],
+		) {}
+	}
 
-	export type History = {
-		commits: Commit[];
-	};
+	export class History {
+		constructor(public commits: Commit[]) {}
+	}
+}
+
+function absurd(clarification: string = ""): never {
+	throw new Error(
+		"This branch of code is supposed to be impossible." + clarification,
+	);
+}
+
+function assert(condition: boolean, or: string = "Absurd"): asserts condition {
+	if (!condition) {
+		throw new Error(or);
+	}
 }
