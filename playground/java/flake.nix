ef4f7979f9f960440f5c187bbce20506aeffbdbf
vv3rd <lyudskoy@gmail.com>
2025-12-14 23:44:53 +0500

    Trying to build java nix package

--- playground/java/flake.nix
+++ playground/java/flake.nix
@@ -1,16 +1,16 @@
 {
-  inputs =
-  {
+  inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in
-  {
-    devShells.${system}.default = pkgs.mkShell {
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
         packages = [
           pkgs.jdk
           pkgs.java-language-server
@@ -21,5 +21,21 @@
           echo "$(java --version)" | ${pkgs.cowsay}/bin/cowsay | ${pkgs.lolcat}/bin/lolcat
         '';
       };
-  };
+
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "simple-http-java";
+        src = ./.;
+        buildInputs = [ pkgs.jdk ];
+        buildPhase = ''
+          javac ./App.java;
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          echo "#! ${pkgs.stdenv.shell}" >> "pkg"
+          echo "exec ${pkgs.jdk}/bin/java $out/bin/App.class" >> "pkg"
+          chmod 0755 "pkg"
+          cp pkg $out/bin/simple-http-java
+        '';
+      };
+    };
 }
