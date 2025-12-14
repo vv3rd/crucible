679ccd67198d363bc6991c79bf7f237060ffdb0b
vv3rd <lyudskoy@gmail.com>
2025-12-14 23:57:58 +0500

    Having fun with setting up haskell playground

--- playground/haskell/flake.nix
+++ playground/haskell/flake.nix
@@ -1,24 +1,24 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { nixpkgs, utils, ... }:
-    utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShell = pkgs.mkShell {
-          packages = [
-            pkgs.ghc
-            pkgs.entr
-            pkgs.just
-          ];
-        };
-      }
-    );
+  outputs = inputs: rec {
+    pkgs = import inputs.nixpkgs {
+      system = "x86_64-linux";
+    };
+    system = pkgs.stdenv.hostPlatform.system;
+
+    devShell.${system} = pkgs.mkShell {
+      packages = [
+        pkgs.ghc
+        pkgs.entr
+        pkgs.just
+      ];
+
+      shellHook = ''
+          just | ${pkgs.lib.getExe pkgs.cowsay} | ${pkgs.lib.getExe pkgs.lolcat}
+        '';
+    };
+  };
 }
