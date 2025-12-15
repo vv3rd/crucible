77b36fc9913a8fac3dfd17c4822b732422bb2613
vv3rd <lyudskoy@gmail.com>
2025-12-16 01:11:25 +0500

    Having fun with haskell

--- playground/haskell/main.hs
+++ playground/haskell/main.hs
@@ -1,8 +1,50 @@
 
 main = do
-    fileText <- readFile "main.hs"
-    let indentedLines = map markIndent fileLines where
-          markIndent = ("----" ++)
-          fileLines = lines fileText
+    putStrLn $ unlines $ withBoldBorder $ withEqualPadding ["Greetings","World"]
 
-    putStrLn $ unlines indentedLines  
+withEqualPadding = withBorder paddingTemplate
+
+withThinBorder = withBorder borderTemplateThin
+
+withBoldBorder =  withBorder borderTemplateBold
+
+borderTemplateThin = borderTemplate
+   "┌─┐"
+   "│ │"
+   "└─┘" 
+
+borderTemplateBold = borderTemplate
+   "╔═╗"
+   "║ ║"
+   "╚═╝"
+
+paddingTemplate = borderTemplate
+   "   "
+   "   "
+   "   "
+
+withBorder:: BorderTemplate -> [String] -> [String]
+withBorder template lines = borderTop ++ map bordered lines ++ borderBottom
+  where
+    ((tl, tc, tr), (cl, cc, cr), (bl, bc, br)) = template
+    maxLength = maximum (map length lines)
+    boxWidth = maxLength + 2
+    borderTop     = [tl : replicate boxWidth tc ++ [tr]]
+    borderBottom  = [bl : replicate boxWidth bc ++ [br]]
+    bordered line = [cl, cc] ++ line ++ replicate (maxLength - length line) cc ++ [cc, cr]
+
+type BorderTemplate =
+  ( (Char, Char, Char)
+  , (Char, Char, Char)
+  , (Char, Char, Char) )
+
+borderTemplate :: String -> String -> String -> BorderTemplate
+borderTemplate row1 row2 row3 =
+  let
+    cells [cell1, cell2, cell3] = (cell1, cell2, cell3)
+  in
+  ( cells row1
+  , cells row2
+  , cells row3
+  )
+
